38f7579f3df00f9b04e260ae62029f12a0a4a832
Gibson Berglund <berglundgibson@gmail.com>
2022-12-04 16:15:20 -0600

    added previous searches to sidebar, and content holders for main display

--- assets/script.js
+++ assets/script.js
@@ -6,12 +6,39 @@ var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + searched + 
 
 var search = document.querySelector('#searchbtn');
 
+var cityList = document.querySelector('#citylist');
+
+var mainDisplay = document.querySelector('#maindisplay');
+var displayLabel = document.querySelector('#displaylabel');
+var displayTemp = document.querySelector('#temp');
+var displayWind = document.querySelector('#wind');
+var displayHum = document.querySelector('#humidity');
+var displayIcon = document.querySelector('#icon');
+
+/*
 search.querySelector('click', function(){
     fetch(queryURL)
     .then(function(response){
         return console.log(response.JSON);
 })});
+*/
 
+//when search button is clicked...
+search.addEventListener('click', function(){
+    console.log(searched.value);
+//create a new card in sidebar with city name
+    var searchedCity = document.createElement('li');
+    searchedCity.textContent = searched.value;
+    searchedCity.setAttribute('class', 'citycard');
+//add card to list of searched cities
+    cityList.appendChild(searchedCity);
+//add city name to main display
+    displayLabel.textContent = searched.value;
+    displayIcon.textContent = '☀️';
+    displayTemp.textContent = 'Temp: ';
+    displayWind.textContent = 'Wind: ';
+    displayHum.textContent = 'Humidity: ';
+});
 
 //list of searched cities appears in sidebar
 /*
